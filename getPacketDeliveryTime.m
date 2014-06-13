840b113000bfdd121d3f80837252ca8eb54a7f9d
Rui Wang <wruiwr86@gmail.com>
2014-06-13 14:28:30 +0200

    add missing files

---
+++ getPacketDeliveryTime.m
@@ -0,0 +1,16 @@
+function [ packet_delivery_time, number_hops ] = getPacketDeliveryTime( protocol )
+%GETPACKETDELIVERYTIME Summary of this function goes here
+%   Detailed explanation goes here
+
+delivery_time_and_hops = [];
+numOfNetwork = size(protocol,2);
+for i = 1:numOfNetwork
+    delivery_time_and_hops(1,i) = str2double(protocol(i).networkID.DelayT);
+    delivery_time_and_hops(2,i) = str2double(protocol(i).networkID.maxHops);
+end
+
+processedData = dataProcess(delivery_time_and_hops);
+packet_delivery_time = average(processedData(1,:));
+number_hops = average(processedData(2,:));
+end
+
